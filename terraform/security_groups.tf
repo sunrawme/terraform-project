4640a8997a23006032706d682aeabd38689ceda7
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-06-16 12:11:48 +0530

    Add security groups for ALB and SonarQube

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -28,3 +28,94 @@ resource "aws_security_group" "bastion_sg" {
     Name = "bastion-sg"
   }
 }
+
+resource "aws_security_group" "alb_sg" {
+
+  name_prefix = "alb-"
+
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+
+    from_port = 80
+    to_port   = 80
+
+    protocol = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+
+    from_port = 443
+    to_port   = 443
+
+    protocol = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+
+    from_port = 0
+    to_port   = 0
+
+    protocol = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
+resource "aws_security_group" "sonarqube_sg" {
+
+  name_prefix = "sonarqube-"
+
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+
+    description = "SSH from Bastion"
+
+    from_port = 22
+    to_port   = 22
+
+    protocol = "tcp"
+
+    security_groups = [
+      aws_security_group.bastion_sg.id
+    ]
+  }
+
+  ingress {
+
+    description = "SonarQube from ALB"
+
+    from_port = 9000
+    to_port   = 9000
+
+    protocol = "tcp"
+
+    security_groups = [
+      aws_security_group.alb_sg.id
+    ]
+  }
+
+  egress {
+
+    from_port = 0
+    to_port   = 0
+
+    protocol = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sonarqube-sg"
+  }
+}
+
