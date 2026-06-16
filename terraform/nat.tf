3324609349740dfaceb62c55751149cc40def960
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-06-16 12:07:27 +0530

    Add NAT gateway resource to nat.tf

--- terraform/nat.tf
+++ terraform/nat.tf
@@ -7,3 +7,18 @@ resource "aws_eip" "nat_eip" {
     Name = "nat-eip"
   }
 }
+
+resource "aws_nat_gateway" "nat" {
+
+  allocation_id = aws_eip.nat_eip.id
+
+  subnet_id = aws_subnet.public_a.id
+
+  depends_on = [
+    aws_internet_gateway.igw
+  ]
+
+  tags = {
+    Name = "sonarqube-nat"
+  }
+}
