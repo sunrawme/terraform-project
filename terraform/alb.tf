783d57e81cb9531b10bb57bf585b1cf7fbbaa683
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-06-16 12:17:08 +0530

    Add AWS Application Load Balancer resource

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,1 +1,17 @@
+resource "aws_lb" "sonarqube_alb" {
 
+  name = "sonarqube-alb"
+
+  load_balancer_type = "application"
+
+  internal = false
+
+  security_groups = [
+    aws_security_group.alb_sg.id
+  ]
+
+  subnets = [
+    aws_subnet.public_a.id,
+    aws_subnet.public_b.id
+  ]
+}
