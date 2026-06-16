51d6519cd8d2997e074dd0c38949beed633922f8
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-06-16 11:49:03 +0530

    Add public subnet resource in subnets.tf

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -1,1 +1,14 @@
+resource "aws_subnet" "public_a" {
 
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = "10.0.1.0/24"
+
+  availability_zone = "ap-south-1a"
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-a"
+  }
+}
