0540e6d3c56766fedac284d7f660d18cd18d53d4
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-06-16 12:01:25 +0530

    Add private subnet resource for database

--- terraform/igw.tf
+++ terraform/igw.tf
@@ -1,1 +1,11 @@
 
+resource "aws_subnet" "private_db_b" {
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.22.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "private-db-b"
+  }
+}
