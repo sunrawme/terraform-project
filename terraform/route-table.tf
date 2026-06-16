d249b56578d6e59d8ece296a0ea6105f6953dd90
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-06-16 12:08:23 +0530

    Add route table associations for public and private subnets

--- terraform/route-table.tf
+++ terraform/route-table.tf
@@ -11,3 +11,54 @@ resource "aws_route_table" "public_rt" {
     Name = "public-rt"
   }
 }
+
+resource "aws_route_table_association" "public_a" {
+
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_b" {
+
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table" "private_rt" {
+
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private_app_a" {
+
+  subnet_id      = aws_subnet.private_app_a.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_app_b" {
+
+  subnet_id      = aws_subnet.private_app_b.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_db_a" {
+
+  subnet_id      = aws_subnet.private_db_a.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_db_b" {
+
+  subnet_id      = aws_subnet.private_db_b.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
