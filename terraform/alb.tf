33df852ecaaa45edfbe4e2429855dce2f30856b9
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-06-16 12:17:36 +0530

    Add AWS LB target group for SonarQube

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -15,3 +15,23 @@ resource "aws_lb" "sonarqube_alb" {
     aws_subnet.public_b.id
   ]
 }
+
+resource "aws_lb_target_group" "sonarqube_tg" {
+
+  name = "sonarqube-tg"
+
+  port = 9000
+
+  protocol = "HTTP"
+
+  vpc_id = aws_vpc.main.id
+
+  health_check {
+
+    path = "/"
+
+    protocol = "HTTP"
+
+    port = "9000"
+  }
+}
