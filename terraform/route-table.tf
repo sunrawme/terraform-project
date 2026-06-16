334f0185bf83f5492d65d0da01618d5d3a133857
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-06-16 12:07:47 +0530

    Add public route table resource in Terraform

--- terraform/route-table.tf
+++ terraform/route-table.tf
@@ -1,1 +1,13 @@
+resource "aws_route_table" "public_rt" {
 
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
