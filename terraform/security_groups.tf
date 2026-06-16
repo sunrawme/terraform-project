a71e4dafe8cd31f6402fd7f59a68963075364cf9
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-06-16 12:11:15 +0530

    Add AWS security group for bastion host

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,1 +1,30 @@
+resource "aws_security_group" "bastion_sg" {
 
+  name_prefix = "bastion-"
+
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+
+    from_port = 0
+    to_port   = 0
+
+    protocol = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "bastion-sg"
+  }
+}
