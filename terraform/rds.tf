7abb6b20f4f2cf6e83ce68b08f58bd5a4d57af2c
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-06-16 12:16:08 +0530

    Add RDS resources for PostgreSQL database

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,1 +1,78 @@
+resource "aws_security_group" "postgres_sg" {
 
+  name_prefix = "postgres-"
+
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+
+    from_port = 5432
+    to_port   = 5432
+
+    protocol = "tcp"
+
+    security_groups = [
+      aws_security_group.sonarqube_sg.id
+    ]
+  }
+
+  egress {
+
+    from_port = 0
+    to_port   = 0
+
+    protocol = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "postgres-sg"
+  }
+}
+
+
+resource "aws_db_subnet_group" "postgres" {
+
+  name = "postgres-subnet-group"
+
+  subnet_ids = [
+    aws_subnet.private_db_a.id,
+    aws_subnet.private_db_b.id
+  ]
+
+  tags = {
+    Name = "postgres-subnet-group"
+  }
+}
+
+resource "aws_db_instance" "postgres" {
+
+  identifier = "sonarqube-postgres"
+
+  engine = "postgres"
+
+  engine_version = "16"
+
+  instance_class = "db.t3.micro"
+
+  allocated_storage = 20
+
+  username = "sonar"
+
+  password = "ChangeThisPassword123!"
+
+  db_name = "sonarqube"
+
+  multi_az = true
+
+  publicly_accessible = false
+
+  skip_final_snapshot = true
+
+  db_subnet_group_name = aws_db_subnet_group.postgres.name
+
+  vpc_security_group_ids = [
+    aws_security_group.postgres_sg.id
+  ]
+}
